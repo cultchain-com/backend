b3d317f82b6f82e2515d7c83dfacd5bc544e7202
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-10-29 16:23:07 +0300

    User profile added

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    USER_TYPES = [
+        ('Personal', 'Personal'),
+        ('Corporate', 'Corporate')
+    ]
+
+    user_type = models.CharField(max_length=10, choices=USER_TYPES, default='Personal')
+    wallet_address = models.CharField(max_length=42, unique=True)  # Ethereum address length
+    name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(unique=True, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    # User type
+    is_donor = models.BooleanField(default=False)
+    is_validator = models.BooleanField(default=False)
+    is_creator = models.BooleanField(default=False)
+
+    # social links
+    twitter_link = models.URLField(blank=True, null=True)
+    facebook_link = models.URLField(blank=True, null=True)
+    instagram_link = models.URLField(blank=True, null=True)
+    linkedin_link = models.URLField(blank=True, null=True)
+
+    # Fields specific to corporate users
+    company_name = models.CharField(max_length=255, blank=True, null=True)
+    company_registration_number = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.wallet_address
