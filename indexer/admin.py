339744e84ed5a21590a57c56f4927f698e0618d4
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-10-29 16:25:02 +0300

    Admin template updated

--- indexer/admin.py
+++ indexer/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Milestone)
+admin.site.register(Committee)
+admin.site.register(CommitteeMembership)
+admin.site.register(ValidatorRequest)
+admin.site.register(Donation)
+admin.site.register(WithdrawRequest)
+admin.site.register(FundRelease)
