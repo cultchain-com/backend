1897c3a2d7c6774f1bcddae5ac1347ac2fa894a0
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-11-10 18:50:04 +0300

    model updated to support ckeditor

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import BlogPost, BlogImage
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.db import models
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'header_image', 'created_at')
     search_fields = ('title', 'body')
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorUploadingWidget},
+    }
 
 @admin.register(BlogImage)
 class BlogImageAdmin(admin.ModelAdmin):
